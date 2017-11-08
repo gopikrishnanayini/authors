b22b05380648410458e528672282e489c5fdad8e
sushiltech123 <nayinigopi93@gmail.com>
2017-11-08 19:32:00 +0530

    roles added and home controller created and authentication added to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ActiveRecord::Base
   rolify
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  def get_super_admin
+    User.where(:id => self.id).first
+  end 
+  def add_licensee_role
+    if self.license == true
+      self.add_role :licensee_admin
+    end
+  end
 end
